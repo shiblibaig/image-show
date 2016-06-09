9da7c0f39186dfec36bf85c3d8ba53841bd09080
shiblibaig <shiblibaig200@gmail.com>
2016-06-09 22:41:22 +0530

    Update Suave-v3.js

--- Suave-v3.js
+++ Suave-v3.js
@@ -3,6 +3,9 @@ var img = document.getElementById('white');
 var w = img.clientWidth;
 var h = img.clientHeight;
 
+img.height = h/10;
+img.width = w/10;
+
 document.getElementById('myCanvas1-4-1').setAttribute('width',w/2);
 document.getElementById('myCanvas1-4-1').setAttribute('height',h/2);
 
@@ -35,14 +38,27 @@ $("#div6").css('margin-top',-h/2-4);
 $("#div7").css('margin-left', 3*w/4+9);
 $("#div7").css('margin-top', -3);
 
+var check =0;
 
+function change1(){
+	check =1;
+}
+
+function change2(){
+	check =2;
+}
 
 $(document).ready(function(){
 
 	var a=1;
 
 	$("#mainDiv").on('click', function(){
-		if(a==1){
+		
+		if(check==0){
+			alert("Choose a type!!");
+		}
+
+		if(a==1 && check ==1){
 			
 			$("#div5,#div3,#div4,#div6,#div7").velocity({translateX: -w/2-4}, {duration: 400});
 			$("#div1,#div2").velocity({opacity: 0}, {duration: 100});
@@ -53,10 +69,10 @@ $(document).ready(function(){
 			$("#div7").velocity({translateY: -3*h/4-4}, { duration: 400});
 			$("#div6").velocity({opacity: 0}, {duration: 100});
 			
-			a=2;
+			a=2; check=1;
 		}
 
-		else if(a==2){
+		else if(a==2 && check ==1){
 			
 			$("#div6").velocity({opacity: 1}, {duration: 100});
 			$("#div7").velocity({translateY: 0}, { duration: 400});
@@ -70,6 +86,44 @@ $(document).ready(function(){
 			a=1;
 		}
 
+		if(a==1 && check == 2){
+			
+			$("#div5,#div3,#div4,#div6,#div7").velocity({translateX: -w/2-4}, {duration: 400});
+			$("#div1").velocity({scale: 0}, {duration: 100});
+			$("#div2").velocity({scale: 0}, {duration: 100});
+			// $("#div1,#div2").velocity({opacity: 0}, {duration: 100});
+			$("#div5,#div7,#div6").velocity({translateY: -h/2-4}, { duration: 400});
+			// $("#div4,#div3").velocity({opacity: 0}, {duration: 100});
+			$("#div3").velocity({scale: 0}, {duration: 100});
+			$("#div4").velocity({scale: 0}, {duration: 100});
+			$("#div7,#div6").velocity({translateX: -3*w/4-4}, { duration: 400});
+			// $("#div5").velocity({opacity: 0}, {duration: 1400});
+			$("#div5").velocity({scale: 0}, {duration: 100});
+			$("#div7").velocity({translateY: -3*h/4-4}, { duration: 400});
+			// $("#div6").velocity({opacity: 0}, {duration: 1400});
+			$("#div6").velocity({scale: 0}, {duration: 100});
+			
+			a=2; check=2;
+		}
+		else if(a==2 && check == 2){
+
+			// $("#div6").velocity({opacity: 1}, {duration: 100});
+			$("#div6").velocity({scale: 1}, {duration: 100});
+			$("#div7").velocity({translateY: 0}, { duration: 400});
+			// $("#div5").velocity({opacity: 1}, {duration: 100});
+			$("#div5").velocity({scale: 1}, {duration: 200});
+			$("#div7,#div6").velocity({translateX: 0}, { duration: 400});
+			// $("#div4,#div3").velocity({opacity: 1}, {duration: 100});
+			$("#div3").velocity({scale: 1}, {duration: 300});
+			$("#div4").velocity({scale: 1}, {duration: 300});
+			$("#div5,#div7,#div6").velocity({translateY: 0}, { duration: 400});
+			// $("#div1,#div2").velocity({opacity: 1}, {duration: 100});
+			$("#div1").velocity({scale: 1}, {duration: 400});
+			$("#div2").velocity({scale: 1}, {duration: 400});
+			$("#div5,#div3,#div4,#div6,#div7").velocity({translateX: 0}, {duration: 400});
+			
+			a=1;
+		}
 	});
 });
 
